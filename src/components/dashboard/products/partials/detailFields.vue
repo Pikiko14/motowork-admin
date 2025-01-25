7ea75ca2cfce46c160a59c6209504799685a3f6b
Marcos Hernandez <maalhequi95@gmail.com>
2025-01-25 14:22:17 -0500

    valdiate hex and image on colors

--- src/components/dashboard/products/partials/detailFields.vue
+++ src/components/dashboard/products/partials/detailFields.vue
@@ -51,14 +51,37 @@
     <div class="col-12">
       <label for="">Colores disponibles <span class="text-secondary">*</span></label>
       <div class="color-selector q-mt-sm">
-        <div @click="removeColor(idx)" class="color" v-for="(color, idx) in product.details.colors" :key="idx"
-          :style="{ backgroundColor: color.hex }"></div>
+        <div class="color" v-for="(color, idx) in product.details.colors" :key="idx"
+          :style="{ backgroundColor: color.hex }">
+          <q-menu auto-close max-height="330px" transition-show="rotate" transition-hide="rotate" :offset="[10, 10]">
+            <q-list>
+              <q-item
+                clickable
+                v-if="product.images && product.images.length > 0"
+                v-for="(img, idxImg) in product.images" :key="idxImg"
+                 @click="setColorImage(idx, img.path)"
+              >
+                <q-item-section>
+                  <q-img :src="img.path" :alt="`Imagen del producto ${product.sku}`">
+                    <div class="overflow-img" v-if="color.image === img.path">
+                      <q-icon name="img:/images/check.png"></q-icon>
+                    </div>
+                  </q-img>
+                </q-item-section>
+              </q-item>
+              <q-item>
+                <q-item-section>
+                  <q-btn @click="removeColor(idx)" label="Eliminar" color="secondary" unelevated></q-btn>
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </q-menu>
+        </div>
         <div class="color bordered">
           <q-icon size="14pt" name="img:/images/search_black.svg"></q-icon>
-          <q-menu class="text-center">
+          <q-menu :offset="[10, 10]" class="text-center" transition-show="rotate" transition-hide="rotate">
             <q-color class="shadow-0" v-model="color" no-footer style="max-width: 250px" />
-            <q-btn label="Gurdar" v-close-popup @click="addColor" color="secondary"
-              class="q-mb-md" unelevated></q-btn>
+            <q-btn label="Gurdar" v-close-popup @click="addColor" color="secondary" class="q-mb-md" unelevated></q-btn>
           </q-menu>
         </div>
       </div>
@@ -93,6 +116,10 @@ const addColor = (hex: string) => {
     image: ''
   }
   props.product.details.colors.push(colorobj)
+}
+
+const setColorImage = (colorIdx: number, imgPath: string) => {
+  props.product.details.colors[colorIdx].image = imgPath
 }
 </script>
 

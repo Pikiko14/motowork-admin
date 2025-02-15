c9f7c333b304f8d15ddc6287fdc13cfd18f34ee0
Marcos Hernandez <maalhequi95@gmail.com>
2025-02-15 08:44:04 -0500

    fix import layour on routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,7 +3,7 @@ import { RouteRecordRaw } from "vue-router";
 const routes: RouteRecordRaw[] = [
   {
     path: "/",
-    component: () => import("@/layouts/MainLayout.vue"),
+    component: () => import("src/layouts/MainLayout.vue"),
     children: [
       {
         path: "",
@@ -15,7 +15,7 @@ const routes: RouteRecordRaw[] = [
   },
   {
     path: "/dashboard",
-    component: () => import("@/layouts/MainLayout.vue"),
+    component: () => import("src/layouts/MainLayout.vue"),
     children: [
       {
         path: "",
